52406e28e91ef6ed3ebb98ca4b1b4c1e24907e85
Ross <rosstodd@lululemon.com>
2019-08-09 08:47:15 -0700

    chore: Add project boilerplate

---
+++ components/layout.tsx
@@ -0,0 +1,32 @@
+import React, { FunctionComponent } from "react";
+import Link from "next/link";
+import Head from "next/head";
+
+type LayoutProps = {
+  title?: string;
+};
+
+const Layout: FunctionComponent<LayoutProps> = ({ children, title }) => (
+  <div>
+    <Head>
+      <title>{title}</title>
+      <meta charSet="utf-8" />
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+    </Head>
+    <header>
+      <nav>
+        <Link href="/">
+          <a>Home</a>
+        </Link>{" "}
+        |{" "}
+        <Link href="/about">
+          <a>About</a>
+        </Link>{" "}
+        |{" "}
+      </nav>
+    </header>
+    {children}
+  </div>
+);
+
+export default Layout;
---
+++ pages/index.tsx
@@ -0,0 +1,10 @@
+import React, { FunctionComponent } from "react";
+import Layout from "../components/layout";
+
+const Index: FunctionComponent = () => (
+  <Layout>
+    <h1>Hello Next.js 👋</h1>
+  </Layout>
+);
+
+export default Index;
